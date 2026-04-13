a10cc9cac32b777d22c99ce4ffe269d4270f2b4a
Julia <yuliya.sandu26@gmail.com>
2026-04-13 11:40:07 +0200

    Update CraftDialog.h

--- CraftDialog.h
+++ CraftDialog.h
@@ -1,1 +1,31 @@
+#ifndef CRAFTDIALOG_H
+#define CRAFTDIALOG_H
 
+#include <QDialog>
+#include <QLabel>
+#include <QPushButton>
+#include "Personazh.h"
+
+class CraftDialog : public QDialog {
+    Q_OBJECT
+public:
+    explicit CraftDialog(const Personazh* p, QWidget* parent = nullptr);
+    
+signals:
+    void printClicked();
+    
+private slots:
+    void onPrint();
+    void onCancel();
+    void saveImage();
+    
+private:
+    const Personazh* person;
+    QLabel* imageLabel;
+    QPixmap currentPixmap;
+    
+    void updateImage();
+    QPixmap loadStaticImage() const;
+};
+
+#endif
