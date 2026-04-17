de14a02aba852e2833c47a90c18508503f10ad73
Julia <yuliya.sandu26@gmail.com>
2026-04-17 11:05:21 +0200

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -1,1 +1,48 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
 
+#include <QMainWindow>
+#include <QTableWidget>
+#include <vector>
+#include "Personazh.h"
+
+QT_BEGIN_NAMESPACE
+class QPushButton;
+class QComboBox;
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void loadFile();
+    void loadJSON();
+    void saveToJson();
+    void onTableRowClicked(int row, int column);
+    void onTableRowHovered(int row, int column);  // <- НОВЫЙ СЛОТ ДЛЯ НАВЕДЕНИЯ
+    void removePersonazh(Personazh* p);
+    void editCell(int row, int column);
+
+private:
+    QTableWidget* table;
+    QPushButton* btnLoad;
+    QPushButton* btnLoadJSON;
+    QPushButton* btnSaveJson;
+    QComboBox* imageModeCombo;
+    std::vector<Personazh*> persons;
+    
+    int previousHoveredRow;  // <- ЗАПОМИНАЕМ ПРЕДЫДУЩУЮ СТРОКУ
+
+    void setupUI();
+    void parseFile(const QString& filename);
+    void parseJSON(const QString& filename);
+    void refreshTable();
+    Personazh* createFromLine(const QString& line);
+    void highlightRow(int row);  // <- ВСПОМОГАТЕЛЬНЫЙ МЕТОД
+};
+
+#endif // MAINWINDOW_H
